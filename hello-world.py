d8fba97d75aaab788f1a9dce29e329fb32269c04
noakash <52347258+Akashno@users.noreply.github.com>
2021-09-13 20:12:14 +0530

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,14 @@
+array = [104,101,108,108,111,32,119,111,114,108,100]
+string = ""
+string = string + "".join(chr(array[0]))
+string = string + "".join(chr(array[1]))
+string = string + "".join(chr(array[2]))
+string = string + "".join(chr(array[3]))
+string = string + "".join(chr(array[4]))
+string = string + "".join(chr(array[5]))
+string = string + "".join(chr(array[6]))
+string = string + "".join(chr(array[7]))
+string = string + "".join(chr(array[8]))
+string = string + "".join(chr(array[9]))
+string = string + "".join(chr(array[10]))
+print(string)
